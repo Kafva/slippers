7e743172793c4b33d49da6907ddbba772cadeed3
Kafva <Martensson.Jonas98@gmail.com>
2020-10-11 10:55:48 +0200

    -

--- plugins/join/Join.py
+++ plugins/join/Join.py
@@ -43,7 +43,7 @@ class Join(Plugin):
         user.x = "0"
         user.y = "0"
         user.frame = "0"
-
+        print("Joining player: {}".format(user))
         self.add(user)
 
     # Functions
@@ -52,6 +52,7 @@ class Join(Plugin):
         """Adds a player in to their new room."""
         int_id = user.get_int_id(self.rooms)
         self.rooms[user.room]["users"].append(user)
+        print("Add player to room: {}".format(user))
 
         # Games
         if self.rooms[user.room]["isGame"] == "true":
